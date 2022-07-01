c80323f0a107af160c1c0a88d84fc12e2eedff66
trond@tactile.dk <trond@tactile.dk>
2022-07-01 08:57:14 +0200

    generated default files.

---
+++ Editor/_AUTOGENERATED_AssemblySetup.cs
@@ -0,0 +1,4 @@
+
+using System.Runtime.CompilerServices;
+[assembly:InternalsVisibleTo("com.tactilegames.uielements-code-behind.editor.tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
