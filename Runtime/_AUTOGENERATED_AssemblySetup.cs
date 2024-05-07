129e008da678e36d4cddc4237aecb767966afa6f
Trond <trond@tactile.dk>
2024-05-07 15:27:37 +0200

    Update template files [skip ci]

--- Runtime/_AUTOGENERATED_AssemblySetup.cs
+++ Runtime/_AUTOGENERATED_AssemblySetup.cs
@@ -1,5 +1,6 @@
 
 using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("com.tactilegames.uielements-code-behind.editor")]
 [assembly: InternalsVisibleTo("com.tactilegames.uielements-code-behind.editor.tests")]
 [assembly: InternalsVisibleTo("com.tactilegames.uielements-code-behind.tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
